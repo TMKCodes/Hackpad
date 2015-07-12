06460eca572ceb9c81afd51e814302dd9819a7d0
Toni Korpela <toni@mussukka.org>
2015-07-12 05:33:51 +0300

    Add registeration page to web client.
    The registeration page is a html form which is used to send the
    username, password and email address of registering user to the server
    for storage.

--- web-client/js/helpers.js
+++ web-client/js/helpers.js
@@ -16,5 +16,218 @@
 		}
 		return false;
 	}
+	$.globalParameters = function(element, parameters) {
+		if(typeof parameters != "undefined") {
+			if(typeof parameters.accesskey != "undefined") {
+				element.setAttribute("accesskey", parameters.accesskey);
+			}
+			if(typeof parameters.contenteditable != "undefined") {
+				element.setAttribute("contenteditable", parameters.contenteditable);
+			}
+			if(typeof parameters.contextmenu != "undefined") {
+				element.setAttribute("contextmenu", parameters.contextmenu);
+			}
+			if(typeof parameters.dir != "undefined") {
+				element.setAttribute("dir", parameters.dir);
+			}
+			if(typeof parameters.draggable != "undefined") {
+				element.setAttribute("draggable", parameters.draggable);
+			}
+			if(typeof parameters.dropzone != "undefined") {
+				element.setAttribute("dropzone", parameters.dropzone);
+			}
+			if(typeof parameters.hidden != "undefined") {
+				element.setAttribute("hidden", parameters.hidden);
+			}
+			if(typeof parameters.inert != "undefined") {
+				element.setAttribute("inert", parameters.inert);
+			}
+			if(typeof parameters.itemid != "undefined") {
+				element.setAttribute("itemid", parameters.itemid);
+			}
+			if(typeof parameters.itemprop != "undefined") {
+				element.setAttribute("itemprop", parameters.itemprop);
+			}
+			if(typeof parameters.itemref != "undefined") {
+				element.setAttribute("itemref", parameters.itemref);
+			}
+			if(typeof parameters.itemscope != "undefined") {
+				element.setAttribute("itemscope", parameters.itemscope);
+			}
+			if(typeof parameters.itemtype != "undefined") {
+				element.setAttribute("itemtype", parameters.itemtype);
+			}
+			if(typeof parameters.lang != "undefined") {
+				element.setAttribute("lang", parameters.lang);
+			}
+			if(typeof parameters.spellcheck != "undefined") {
+				element.setAttribute("spellcheck", parameters.spellcheck);
+			}
+			if(typeof parameters.style != "undefined") {
+				element.setAttribute("style", parameters.style);
+			}
+			if(typeof parameters.title != "undefined") {
+				element.setAttribute("title", parameters.title);
+			}
+			if(typeof parameters.translate != "undefined") {
+				element.setAttribute("translate", parameters.translate);
+			}	
+		}
+		return element;
+	}
+	$.createDiv = function(id, classes, parameters) {
+		div = document.createElement("div");
+		if(typeof id != "undefined") div.id = id;
+		if(typeof classes != "undefined") div.className = classes;
+		div = $.globalParameters(div, parameters);
+		return $(div);
+	}
+	$.createButton = function(id, classes, parameters) {
+		button = document.createElement("button");
+		if(typeof id != "undefined") button.id = id;
+		if(typeof classes != "undefined") button.className = classes;
+		if(typeof parameters != "undefined" && typeof parameters == "object") {
+			if(typeof parameters.autofocus != "undefined") {
+				button.setAttribute("autofocus", parameters.autofocus);
+			}
+			if(typeof parameters.disabled != "undefined") {
+				button.setAttribute("disabled", parameters.disabled);
+			}
+			if(typeof parameters.form != "undefined") {
+				button.setAttribute("form", parameters.form);
+			}
+			if(typeof parameters.formaction != "undefined") {
+				button.setAttribute("formaction", parameters.formaction);
+			}
+			if(typeof parameters.formenctype != "undefined") {
+				button.setAttribute("formenctype", parameters.formenctype);
+			}
+			if(typeof parameters.formmethod != "undefined") {
+				button.setAttribute("formmethod", parameters.formmethod);
+			}
+			if(typeof parameters.formnovalidate != "undefined") {
+				button.setAttribute("formnovalidate", parameters.formnovalidate);
+			}
+			if(typeof parameters.formtarget != "undefined") {
+				button.setAttribute("formtarget", parameters.formtarget);
+			}
+			if(typeof parameters.menu != "undefined") {
+				button.setAttribute("menu", parameters.menu);
+			}
+			if(typeof parameters.type != "undefined") {
+				button.setAttribute("type", parameters.type);
+			}
+			if(typeof parameters.value != "undefined") {
+				button.setAttribute("value", parameters.value);
+			}
+		}
+		button = $.globalParameters(button, parameters);
+		return $(button);
+	}
+	$.createInput = function(id, classes, parameters) {
+		input = document.createElement("input");
+		if(typeof id != "undefined") input.id = id;
+		if(typeof classes != "undefined") input.className = classes;
+		if(typeof parameters != "undefined" && typeof parameters == "object") {
+			if(typeof parameters.accept != "undefined" && typeof parameters.accept == "string") {
+				input.setAttribute("accept", parameters.accept);
+			}
+			if(typeof parameters.alt != "undefined" && typeof parameters.alt == "string") {
+				input.setAttribute("alt", parameters.alt);
+			}
+			if(typeof parameters.autocomplete != "undefined" && (parameters.autocomplete == "on" || parameters.autocomplete == "off")) {
+				input.setAttribute("autocomplete", parameters.autocomplete);
+			}
+			if(typeof parameters.autofocus != "undefined" && (parameters.autofocus == "autofocus" || parameters.autofocus == "")) {
+				input.setAttribute("autofocus", parameters.autofocus);
+			}
+			if(typeof parameters.checked != "undefined" && typeof parameters.type != "undefined" && (parameters.type == "checkbox" || parameters.type == "radio")) {
+				input.setAttribute("checked", parameters.checked);
+			}
+			if(typeof parameters.disabled != "undefined" && (parameters.disabled == "disabled" || parameters.disabled == "")) {
+				input.setAttribute("disabled", parameters.disabled);
+			}
+			if(typeof parameters.dirname != "undefined") {
+				input.setAttribute("dirname", parameters.dirname);
+			}
+			if(typeof parameters.form != "undefined") {
+				input.setAttribute("form", parameters.form);
+			}
+			if(typeof parameters.formaction != "undefined") {
+				input.setAttribute("formaction", parameters.formaction);
+			}
+			if(typeof parameters.formenctype != "undefined") {
+				input.setAttribute("formenctype", parameters.formenctype);
+			}
+			if(typeof parameters.formmethod != "undefined") {
+				input.setAttribute("formmethod", parameters.formmethod);
+			}
+			if(typeof parameters.formnovalidate != "undefined") {
+				input.setAttribute("formnovalidate", parameters.formnovalidate);
+			}
+			if(typeof parameters.formtarget != "undefined") {
+				input.setAttribute("formtarget", parameters.formtarget);
+			}
+			if(typeof parameters.height != "undefined") {
+				input.setAttribute("height", parameters.height);
+			}
+			if(typeof parameters.inputmode != "undefined") {
+				input.setAttribute("inputmode", parameters.inputmode);
+			}
+			if(typeof parameters.list != "undefined") {
+				input.setAttribute("list", parameters.list);
+			}
+			if(typeof parameters.max != "undefined") {
+				input.setAttribute("max", parameters.max);
+			}
+			if(typeof parameters.maxlength != "undefined") {
+				input.setAttribute("maxlength", parameters.maxlength);
+			}
+			if(typeof parameters.min != "undefined") {
+				input.setAttribute("min", parameters.min);
+			}
+			if(typeof parameters.minlength != "undefined") {
+				input.setAttribute("minlength", parameters.minlength);
+			}
+			if(typeof parameters.multiple != "undefined") {
+				input.setAttribute("multiple", parameters.multiple);
+			}
+			if(typeof parameters.name != "undefined") {
+				input.setAttribute("name", parameters.name);
+			}
+			if(typeof parameters.pattern != "undefined") {
+				input.setAttribute("pattern", parameters.pattern);
+			}
+			if(typeof parameters.placeholder != "undefined") {
+				input.setAttribute("placeholder", parameters.placeholder);
+			}
+			if(typeof parameters.readonly != "undefined") {
+				input.setAttribute("readonly", parameters.readonly);
+			}
+			if(typeof parameters.required != "undefined") {
+				input.setAttribute("required", parameters.required);
+			}
+			if(typeof parameters.size != "undefined") {
+				input.setAttribute("size", parameters.size);
+			}
+			if(typeof parameters.src != "undefined") {
+				input.setAttribute("src", parameters.src);
+			}
+			if(typeof parameters.step != "undefined") {
+				input.setAttribute("step", parameters.step);
+			}
+			if(typeof parameters.type != "undefined") {
+				input.setAttribute("type", parameters.type);
+			}
+			if(typeof parameters.value != "undefined") {
+				input.setAttribute("value", parameters.value);
+			}
+			if(typeof parameters.width != "undefined") {
+				input.setAttribute("width", parameters.width);
+			}
+		}
+		input = $.globalParameters(input, parameters);
+		return $(input);
+	}
 	
 }(jQuery));
--- web-client/main.js
+++ web-client/main.js
@@ -1,63 +1,113 @@
 var application = {
+	hostname : "",
 	authentication : {
 		open : function() {
-			$("document").attr("title", "Fudocs Kirjautuminen.");
-			var container = $.create("div", "authentication-container");
-			
-			var header = $.create("h1", "authentication-header");
-			$(header).html("Fudocs Markdown Editor");
-
-			var form = $.create("form", "authentication-form");
-			
-			var usernameInput = $.create("input", "authentication-form-username-input");
-			$(usernameInput).attr("placeholder", "Käyttäjätunnus");
-			$(usernameInput).attr("required", "required");
-			$(usernameInput).attr("name", "username");
-			$(usernameInput).attr("type", "text");
-
-			var passwordInput = $.create("input", "authentication-form-password-input");
-			$(passwordInput).attr("placeholder", "Salasana");
-			$(passwordInput).attr("required", "required");
-			$(passwordInput).attr("name", "password");
-			$(passwordInput).attr("type", "password");
-
-			var submitInput = $.create("input", "authentication-form-submit-input");
-			$(submitInput).attr("value", "Kirjaudu ja dokumentoi!");
-			$(submitInput).attr("name", "submit");
-			$(submitInput).attr("type", "submit");
-
-			var registerationButton = $.create("input", "authentication-registeration-button"); 
-			$(registerationButton).attr("value", "Tarvitsetko käyttäjätunnuksen?");
-			$(registerationButton).attr("name", "reset");
-			$(registerationButton).attr("type", "reset");
-
-			$(form).append(usernameInput);
-			$(form).append(passwordInput);
-			$(form).append(submitInput);
-			$(form).append(registerationButton);
-
-			$(container).append(header);
-			$(container).append(form);
-
-
-			$("body").prepend(container);
-
+			$(document).attr("title", "Fudocs Kirjautuminen.");
+			history.pushState({ application : "authentication" }, "", application.hostname + "?application=authentication");
+			$("body").prepend($.createDiv("authentication-container"));
+			$("#authentication-container").append($.create("h1", "authentication-header"));
+			$("#authentication-header").html("Fudocs Markdown Editor");
+			$("#authentication-container").append($.create("form", "authentication-form"));
+			$("#authentication-form").append($.createInput("authentication-form-username-input", null, { 
+					placeholder : "Käyttäjätunnus", required : "required", name : "username", type : "text"}));
+			$("#authentication-form").append($.createInput("authentication-form-password-input", null, { 
+					placeholder : "Salasana", required : "required", name : "password", type : "password"}));
+			$("#authentication-form").append($.createInput("authentication-form-submit-input", null, { 
+					value : "Kirjaudu ja dokumentoi", name : "submit", type : "submit"}));
+			$("#authentication-form").append($.createInput("authentication-registeration-button", null, { 
+					value : "Tarvitsetko käyttäjätunnuksen", name : "reset", type : "reset"}));
 			$("#authentication-form").submit(function(evt) {
 				evt.preventDefault();
 				var session = fudocs.session.create($("#authentication-form-username-input").val(), $("#authentication-form-password-input").val());
 				console.log(session);
 			});
+
+			$("#authentication-registeration-button").click(function(evt) {
+				application.authentication.close();
+				application.registeration.open();
+			});
 		},
 		close : function() {
 			$("#authentication-container").remove();
 		}
+	},
+	registeration : {
+		open : function() {
+			$(document).attr("title", "Rekisteröidy Fudocsiin.");
+			history.pushState({ application : "registeration" }, "", application.hostname + "?application=registeration");
+			$("body").prepend($.createDiv("registeration-container"));
+			$("#registeration-container").append($.create("h1", "registeration-header"));
+			$("#registeration-header").html("Rekisteröi Fudocs käyttäjätunnus.");
+			$("#registeration-container").append($.create("form", "registeration-form"));
+			$("#registeration-form").append($.createInput("registeration-form-username-input", null, { 
+					placeholder : "Käyttäjätunnus", required : "required", name : "username", type : "text" }));
+			$("#registeration-form").append($.createInput("registeration-form-password-input", null, {
+					placeholder : "Salasana", required : "required", name : "password", type : "password" }));
+			$("#registeration-form").append($.createInput("registeration-form-password-confirm-input", null, {
+					placeholder : "Salasanan varmistus", required : "required", name : "password-confirm", type : "password" }));
+			$("#registeration-form").append($.createInput("registeration-form-email-input", null, {
+					placeholder : "Sähköpostiosoite", required : "required", name : "email-address", type : "email" }));
+			$("#registeration-form").append($.createInput("registeration-form-submit-button", null, {
+					value : "Rekisteröidy nyt!", name : "submit", type : "submit" }));
+			$("#registeration-form").append($.createInput("registeration-form-cancel-button", null, {
+					value : "Keskeytä rekisteröityminen.", name : "reset", type : "reset" }));
+			$("#registeration-form").submit(function(evt) {
+				evt.preventDefault();
+				if($("#registeration-form-password-input").val() == $("#registeration-form-password-confirm-input").val()) {
+					var username = $("#registeration-form-username-input").val();
+					var password = $("#registeration-form-password-input").val();
+					var email = $("#registeration-form-email-input").val();
+					if(fudocs.account.create(username, password, email) == true) {
+						application.closeAll();
+						application.authentication.open();
+						alert("Rekisteröityminen onnistui.");
+					} else {
+						alert("Rekisteröityminen epäonnistui.");
+					}
+				} else {
+					alert("Salasana varmistus ei vastaa annettua salasanaa.");
+				}
+			});
+			$("#registeration-form-cancel-button").click(function(evt) {
+				application.closeAll();
+				application.authentication.open();
+			});
+		},
+		close : function() {
+			$("#registeration-container").remove();
+		}
+	},
+	openByState : function(state) {
+		console.log(state.application);
+		if(state.application == "authentication") {
+			application.authentication.open();
+		} else if(state.application == "registeration") {
+			application.registeration.open();
+		}
+	},
+	openByURL : function() {
+		if($.getUrlParameter("application") == false) {
+			application.authentication.open();
+		} else if($.getUrlParameter("application") == "authentication") {
+			application.authentication.open();
+		} else if($.getUrlParameter("application") == "registeration") {
+			application.registeration.open();
+		}
+	},
+	closeAll : function() {
+		application.authentication.close();
+		application.registeration.close();
 	}
 }
 
 $("document").ready(function() {
-	console.log("Welcome to Fudocs. This is developer console!");
+	application.hostname = location.origin + location.pathname;
+	console.log("Welcome to Fudocs at " + application.hostname + ". This is developer console!");
 	console.log("API Endpoint: " + config.serverEndpoint);
-	if($.getUrlParameter("page") == false) {
-		application.authentication.open();
+	application.openByURL();
+	window.onpopstate = function(evt) {
+		history.back();
+		application.closeAll();
+		application.openByState(evt.state);
 	}
 });
