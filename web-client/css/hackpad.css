a45bf35d69aefeedbe23d036250d2083f724feb0
Toni Korpela <toni@mussukka.org>
2015-07-26 04:06:21 +0300

    change hackpad.js back to css file

---
+++ web-client/css/hackpad.css
@@ -0,0 +1,215 @@
+html {
+	font-family: 'Open Sans', Sans-Serif;
+	background-color: rgba(255,250,250, 1);
+}
+
+body {
+	margin: 0px 0px 0px 0px;
+}
+
+input, textarea, button {
+	border: 5px solid white;
+	-webkit-box-shadow: inset 0 0 8px rgba(0,0,0,0.1), 0 0 16px rgba(0,0,0,0.1);
+	-moz-box-shadow: inset 0 0 8px rgba(0,0,0,0.1), 0 0 16px rgba(0,0,0,0.1);
+	-box-shadow: inset 0 0 8px rgba(0,0,0,0.1), 0 0 16px rgba(0,0,0,0.1);
+	padding: 2px 6px 2px 6px;
+	margin: 0 0 10px 0;
+	background-color: rgba(255, 250, 250, 0.9)
+	-webkit-box-sizing: content-box;
+	-moz-box-sizing: content-box;
+	box-sizing: content-box;
+
+}
+
+.container {
+	margin-left: auto;
+	margin-right: auto;
+	width: 1024px;
+}
+
+
+.container > h1 {
+	margin-left: auto;
+	margin-right: auto;
+	text-align: center;
+}
+
+.container > form {
+	margin-left: auto;
+	margin-right: auto;
+	display: block;
+	overflow: none;
+	max-width: 380px;
+}
+
+.container > form > input, textarea, button {
+	width: calc(100% - 24px);
+}
+
+.editor {
+	width: 100%;
+}
+
+.editor > #editor-menu-area {
+	background-color: #f5f5f5;
+	width: 100%;
+	height: 41px;
+	float: left;
+}
+
+.editor > #editor-menu-area > #editor-navigation {
+	position: relative;
+	height: 100%;
+	float: left;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root {
+	margin: 0px auto;
+	position: relative;
+	float: left;
+	border-left: 1px solid #c4dbe7;
+	border-right: 1px solid #c4dbe7;
+	list-style-type: none;
+	-webkit-padding-start: 0px;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root li {
+	display: inline;
+	font-size: 12px;
+	font-weight: bold;
+	margin: 0px;
+	padding: 0px;
+	float: left;
+	position: relative;
+	border-top: 1px solid #c4dbe7;
+	border-bottom: 2px solid #c4dbe7;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root li a {
+	padding: 10px 25px;
+	color: #616161;
+	text-shadow: 1px 1px 0px #fff;
+	text-decoration: none;
+	display: inline-block;
+	border-right: 1px solid #fff;
+	border-left: 1px solid #c2c2c2;
+	border-top: 1px solid #fff;
+	background-color: #f5f5f5;
+	-webkit-transition: color 0.2s linear, background 0.2s linear;
+	-moz-transition: color 0.2s linear, background 0.2s linear;
+	-o-transition: color 0.2s linear, background 0.2s linear;
+	transition: color 0.2s linear, background 0.2s linear;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root li a:hover {
+	background-color: #f8f8f8;
+	color: #282828;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root li a.first {
+	border-left: 0px none;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root li a.last {
+	border-right: 0px none;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root li:hover > a {
+	background-color: #fff;
+}
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root li:hover > ul {
+	visibility: visible;
+	opacity: 1;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root ul, 
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root ul li ul {
+	list-style: none;
+	margin: 0px;
+	padding: 0px;
+	visibility: hidden;
+	opacity: 0;
+	position: absolute;
+	z-index: 99999;
+	width: 180px;
+	background-color: #f8f8f8;
+	box-shadow: 1px 1px 3px #ccc;
+	-webkit-transition:opacity 0.2s linear, visibility 0.2s linear;
+	-moz-transition:opacity 0.2s linear, visibility 0.2s linear;
+	-o-transition:opacity 0.2s linear, visibility 0.2s linear;
+	transition:opacity 0.2s linear, visibility 0.2s linear;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root ul {
+	top: 43px;
+	left: 1px;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root ul li ul {
+	top: 0px;
+	left: 181px;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root ul li {
+	clear: both;
+	width: 100%;
+	border: 0px none;
+	border-bottom: 1px solid #c9c9c9;
+}
+
+.editor > #editor-menu-area > #editor-navigation > #editor-navigation-root ul li a {
+	background-color: none;
+	padding: 7px 15px;
+	color: #616161;
+	text-shadow: 1px 1px 0px #fff;
+	text-decoration: none;
+	display: inline-block;
+	border: 0px none;
+	float: left;
+	clear: both;
+	width: 150px;
+}
+
+.editor > #editor-edit-area {
+	position: relative;
+	width: 50%;
+	height: calc(100% - 20px - 41px);
+	float: left;
+}
+
+.editor > #editor-edit-area > #editor-textarea {
+	position: relative;
+	padding: 5px 5px 5px 5px;
+	margin: 0px 0px 0px 0px;
+	border: 1px solid black;
+	border-top: none;
+	border-bottom: none;
+	border-left: none;
+	width: calc(100% - 11px);
+	height: calc(100% - 10px);
+	resize: none;
+}
+
+.editor > #editor-edit-area > #editor-textarea:focus {
+	outline: 0px;
+}
+
+.editor > #editor-hidden-area {
+	display: none;
+}
+
+.editor > #editor-display-area {
+	width: calc(50% - 40px);
+	height: calc(100% - 20px - 41px);
+	float: left;
+	padding: 0px 20px 0px 20px;
+	word-break: normal;
+	overflow-y: scroll;
+}
+
+.editor > #editor-bottom-area {
+	background-color: #483C32;
+	width: 100%;
+	height: 20px;
+	float: left;
+}
